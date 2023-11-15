4abf523267fc5965af944c014f57734cf8362d0a
snavy <1337snavy@gmail.com>
2023-11-15 21:51:11 +0000

    complete tasks

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(path, 'utf-8', (err, data) => {
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
-    return;
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
-
