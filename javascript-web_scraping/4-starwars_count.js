8fa7a593dad1864724aeb9f5832f323d6e125a77
snavy <1337snavy@gmail.com>
2023-11-15 22:03:31 +0000

    4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const url = process.argv[2];
-const id = 18;
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
+let count = 0;
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    const bodyParse = JSON.parse(body);
+    const resultList = bodyParse.results;
+    for (let i = 0; i < resultList.length; i++) {
+      const character = resultList[i].characters;
+      for (let j = 0; j < character.length; j++) {
+        if (character[j].endsWith('/18/')) {
+          count += 1;
+        }
+      }
+    }
   }
-  const filmsData = JSON.parse(body).results;
-  const WithWedgeAntilles = filmsData.filter(film =>
-    film.characters.includes(`https://swapi-api.hbtn.io/api/people/${id}/`)
-  );
-
-  console.log(WithWedgeAntilles.length);
+  console.log(count);
 });
